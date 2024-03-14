7274010457f6d7e89567f93b96a54e826518f771
Abbas Khosravi <khosravi1977@gmail.com>
2024-03-14 09:23:38 +0330

    29

---
+++ 29/src/App.jsx
@@ -0,0 +1,31 @@
+
+import { Component } from 'react';
+import  './App.css'
+import Counter from './components/Counter';
+
+class App extends Component {
+render(){
+    return(
+        <div className='App'>
+             <h1>شمارنده من</h1>
+             <br/>
+             <Counter/>
+
+        </div>
+    )
+   
+}
+
+}
+
+// const App=()=>{
+//     return (
+//         <div className="App"> 
+//             <h1>شمارنده من</h1>
+//             <br/>
+//             <Counter/>
+//         </div>
+      
+//     )
+// }
+export default App;
