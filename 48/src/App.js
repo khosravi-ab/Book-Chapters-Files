23f057457df8369cad13883a195f2a3be97d3195
Abbas Khosravi <khosravi1977@gmail.com>
2024-03-20 10:04:38 +0330

    48

---
+++ 48/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App=()=> {
+  return (
+    <div className="App">
+      <h3>اپلیکیشن مدیریت مخاطبین</h3>
+      <span className="fa  fa-user"/>
+      <button className="btn  btn-primary" >ساخت مخاطب جدید</button>
+      
+    </div>
+  );
+}
+
+export default App;
