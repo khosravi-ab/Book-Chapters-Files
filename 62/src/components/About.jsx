b36a7cd81d5fb62d3b4cd2ffccc0504c7a6c2752
Abbas Khosravi <khosravi1977@gmail.com>
2024-03-27 09:27:02 +0330

    62

---
+++ 62/src/components/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+      <div style={{ padding: "1rem 0" }}>
+        <h2>آنچه درباره ما باید بدانید</h2>
+      </div>
+    );
+  };
+  
+  export default About;
+  
