f498fd76a306ee38e5fa910ba22a7d79aa8139ec
Abbas Khosravi <khosravi1977@gmail.com>
2024-03-30 10:12:32 +0330

    71

---
+++ 71/src/App.js
@@ -0,0 +1,65 @@
+import { Component } from "react";
+import "./App.css";
+import Clock from "./components/Clock";
+/*
+ * // NOTE Mounting LifeCycle In Class Components
+ * - constructor()
+ * - static getDerivedStateFromProps(props, state)
+ * - render()
+ * - componentDidMount()
+ */
+class App extends Component {
+  constructor() {
+    super();
+    console.log("App.js - constructor()");
+    this.state = { date: new Date(), showClock: true, color: false };
+
+    this.colorChange = this.colorChange.bind(this);
+    this.clockChange = this.clockChange.bind(this);
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("App.js - getDerivedStateFromProps()");
+    return state;
+  }
+
+  componentDidMount() {
+    console.log("App.js - componentDidMount()");
+
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    console.log("App.js - componentWillUnmount()");
+
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+  clockChange() {
+    this.setState({ showClock: !this.state.showClock });
+  }
+  colorChange() {
+    this.setState({ color: !this.state.color });
+}
+  render() {
+    console.log("App.js - render()");
+    const { date, showClock, color } = this.state;
+    return (
+      <div className="App">
+        <p>چرخه حیات  </p>
+        {showClock ? <Clock date={date} color={color} /> : null}
+        <hr />
+        <button onClick={this.clockChange}>نمایش ساعت</button>
+        <button onClick={this.colorChange}>تغییر رنگ</button>
+
+      </div>
+   );
+
+  }
+}
+
+export default App;
+
