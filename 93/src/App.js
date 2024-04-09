ed38b90d6e7aaadd368c2bd6d8592865b8012513
Abbas Khosravi <khosravi1977@gmail.com>
2024-04-09 08:24:04 +0430

    93

---
+++ 93/src/App.js
@@ -0,0 +1,50 @@
+import Accordion from "./components/Accordion";
+import { UseMemoExample, UseRefExample, UseCallbackExample, UseReducerExample, UseLayoutEffectExample, UseImperativeHandleExample,UseDeferredValueExample,UseTransitionExample,  } from "./hooks";
+const App = () => {
+  return (
+    <div className="container">
+      <div className="text-center mt-2">
+        <h2>یادگیری و استفاده از هوک های ری اکت 🚀</h2>
+      </div>
+      <hr className="text-danger" />
+      <Accordion title="مثال هوک useRef" heading="headingOne">
+        <UseRefExample />
+      </Accordion>
+      <hr className="text-primary" />     
+      <Accordion title="مثال هوک useMemo" heading="headingTwo">
+        <UseMemoExample />
+      </Accordion>
+      <hr className="text-success" />
+      <Accordion title="مثال آموزشی هوک useCallback" heading="headingThree">
+        <UseCallbackExample />
+     </Accordion>
+     <hr className="text-dark" />
+     <Accordion title="مثال آموزشی هوک useReducer" heading="headingFour">
+        <UseReducerExample />
+     </Accordion>
+     <hr className="text-info" />
+     <Accordion title="مثال آموزشی هوک useLayoutEffect" heading="headingFive">
+        <UseLayoutEffectExample />
+     </Accordion>
+     <hr className="text-warning" />
+     <Accordion title="مثال آموزشی هوک useImperativeHandle " heading="headingSix">
+        <UseImperativeHandleExample />
+     </Accordion>
+     <hr className="text-danger" />
+     <Accordion title="آشنایی با هوک useDeferredValue" heading="headingSeven">
+        <UseDeferredValueExample />
+     </Accordion>
+     <hr className="text-primary" />
+     
+     <Accordion title="آشنایی با هوک useTransition" heading="headingEight">
+        <UseTransitionExample />
+     </Accordion>
+     <hr className="text-success" />
+
+
+ 
+    </div>
+  );
+};
+
+export default App;
---
+++ 93/src/hooks/UseReducerExample.jsx
@@ -0,0 +1,58 @@
+import { useReducer } from "react";
+
+/*
+ * NOTE
+ * -- useReducer
+ * 1- reducer -> functions -> handle state
+ * 2- action -> Object -> dispatch to reducer (type, payload)
+ * 3- store -> stores state
+ */
+
+const ACTIONS = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + action.payload };
+    case ACTIONS.DECREMENT:
+      return { count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
+const UseReducerExample = () => {
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  const increment = () => {
+    // setCount(prevCount => prevCount + 1)
+    dispatch({ type: ACTIONS.INCREMENT, payload: 1 });
+  };
+
+  const decrement = () => {
+    // setCount(prevCount => prevCount - 1)
+    dispatch({ type: ACTIONS.DECREMENT, payload: 1 });
+  };
+
+  return (
+    <div className="mx-auto mt-5 d-grid gap-3 w-50">
+      <h5 className="alert alert-info text-center">آشنایی با هوک useReducer</h5>
+      <button className="btn btn-primary" onClick={increment}>
+        اضافه کردن 😉
+      </button>
+      <p className="alert alert-warning text-center">
+        شمارش شما برابر است با :{" "}
+        <span className="badge rounded-pill bg-success">{state.count}</span>
+      </p>
+      <button className="btn btn-danger" onClick={decrement}>
+        کم کردن 🤠
+      </button>
+    </div>
+  );
+};
+
+export default UseReducerExample;
