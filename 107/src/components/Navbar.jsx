47897eb07fb538b0592ea976cc093890f4981363
Abbas Khosravi <khosravi1977@gmail.com>
2024-04-15 07:55:46 +0430

    107

---
+++ 107/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import SearchContact from "./contacts/SearchContact";
+import {BACKGROUND,PURPLE} from "../helpers/color"
+import { useLocation } from "react-router-dom";
+
+const Navbar = () => {
+  const location = useLocation();
+  return (
+    <nav className="navbar navbar-dark navbar-expand-sm shadow-lg" style={{backgroundColor: BACKGROUND}}>
+      <div className="container">
+        <div className="row w-100">
+          <div className="col">
+            <div className="navbar-brand">
+               <i className="fas fa-id-badge" style={{color: PURPLE}} />{" "}
+                 وب اپلیکیشن مدیریت{"  "}
+               <span style={{ color: PURPLE }}>مخاطبین</span>
+            </div>
+            
+          </div>
+          <div className="col">
+          {location.pathname === "/contacts" ? (
+            <div className="col">
+              <SearchContact />
+            </div>
+          ) : null}
+
+
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
