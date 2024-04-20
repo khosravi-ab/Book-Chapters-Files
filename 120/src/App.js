5809a8b30ee874cbf6cbef6f2b02eb6abdae734b
Abbas Khosravi <khosravi1977@gmail.com>
2024-04-20 08:18:11 +0430

    120

---
+++ 120/src/App.js
@@ -0,0 +1,18 @@
+import PortalTest from "./components/PortalTest";
+
+import "./App.css";
+import MyForm from "./components/MyForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>ReactJs Course Season 12</h3>
+      <hr color="tomato" />
+      <PortalTest />
+      <MyForm />
+      <hr color="cyan" />
+    </div>
+  );
+}
+
+export default App;
