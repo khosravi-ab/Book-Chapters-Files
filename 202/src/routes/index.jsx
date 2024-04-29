9fd41e1b75d83c0a04aaa2ebf80264cba6dd3b55
Abbas Khosravi <khosravi1977@gmail.com>
2024-04-29 08:05:08 +0430

    202

---
+++ 202/src/routes/index.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import App from "../App";
+import MainLayout from "../layouts/MainLayout";
+import SingleBlogPage from "../components/SingleBlogPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <h3 className="text-center"> چیزی پیدا نکردیم 🤗 ...</h3>,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/blogs/:blogId",
+        element: <SingleBlogPage />,
+      },
+    ],
+  },
+]);
