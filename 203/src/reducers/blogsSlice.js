bd6b793a3ec9c8e31bb334dc0b3eebb55f2757c1
Abbas Khosravi <khosravi1977@gmail.com>
2024-04-30 08:06:13 +0430

    203

---
+++ 203/src/reducers/blogsSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    id: nanoid(),
+
+    title: "اولین پست",
+    content: "محتوای اولین پست ما ☺️",
+  },
+  {
+    id: nanoid(),
+
+    title: "دومین پست",
+    content: "محتوای دومین پست ما    🤗",
+  },
+];
+
+const blogsSlice = createSlice({
+  name: "blogs",
+  initialState: initialState,
+  reducers: {
+    blogAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+    },
+  },
+});
+export const { blogAdded } = blogsSlice.actions;
+export default blogsSlice.reducer;
---
+++ 203/src/routes/index.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import App from "../App";
+import MainLayout from "../layouts/MainLayout";
+import SingleBlogPage from "../components/SingleBlogPage";
+import CreateBlogForm from "../components/CreateBlogForm";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <h3 className="text-center"> چیزی پیدا نکردیم 🤗 ...</h3>,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/blogs/:blogId",
+        element: <SingleBlogPage />,
+      },
+      {
+        path: "/blogs/create-blog",
+        element: <CreateBlogForm />,
+      },
+    ],
+  },
+]);
