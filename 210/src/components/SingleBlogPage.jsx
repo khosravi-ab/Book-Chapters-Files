162cfad6b8d1e56c266fd229425e66502f524b8e
Abbas Khosravi <khosravi1977@gmail.com>
2024-05-03 07:18:16 +0330

    210

---
+++ 210/src/components/SingleBlogPage.jsx
@@ -0,0 +1,53 @@
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { blogDeleted, selectBlogById } from "../reducers/blogsSlice";
+import ShowTime from "./ShowTime";
+import ShowAuthor from "./ShowAuthor";
+const SingleBlogPage = () => {
+  const { blogId } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const blog = useSelector((state) => selectBlogById(state, blogId));
+
+  if (!blog) {
+    return (
+      <section>
+        <h2>پستی که دنبالش می گردی وجود ندارد 🤗</h2>
+      </section>
+    );
+  }
+
+  const handleDelete = () => {
+    if (blog) {
+      dispatch(blogDeleted({ id: blog.id }));
+      navigate("/");
+    }
+  };
+
+  return (
+    <section>
+      <article className="blog">
+        <h2>{blog.title}</h2>
+        <div style={{ marginTop: "10px", marginRight: "20px" }}>
+          <ShowTime timestamp={blog.date} />
+          <ShowAuthor userId={blog.user} />
+        </div>
+
+        <p className="blog-content">{blog.content}</p>
+        <Link to={`/editBlog/${blog.id}`} className="button">
+          ویرایش پست
+        </Link>
+        <button
+          className="muted-button"
+          style={{ marginRight: "10px" }}
+          onClick={handleDelete}
+        >
+          حذف پست
+        </button>
+      </article>
+    </section>
+  );
+};
+
+export default SingleBlogPage;
---
+++ 210/src/reducers/blogsSlice.js
@@ -0,0 +1,63 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns-jalali";
+const initialState = {
+  blogs: [
+    {
+      id: nanoid(),
+      date: sub(new Date(), { days: 2, minutes: 10 }).toISOString(),
+      title: "اولین پست",
+      content: "محتوای اولین پست ما ☺️",
+      user: "1",
+    },
+    {
+      id: nanoid(),
+      date: sub(new Date(), { minutes: 5 }).toISOString(),
+      title: "دومین پست",
+      content: "محتوای دومین پست ما    🤗",
+      user: "3",
+    },
+  ],
+};
+
+const blogsSlice = createSlice({
+  name: "blogs",
+  initialState: initialState,
+  reducers: {
+    blogAdded: {
+      reducer(state, action) {
+        state.blogs.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        //Complex logic
+        return {
+          payload: {
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            user: userId,
+          },
+        };
+      },
+    },
+    blogUpdated: (state, action) => {
+      const { id, title, content } = action.payload;
+      const existingBlog = state.blogs.find((blog) => blog.id === id);
+      if (existingBlog) {
+        existingBlog.title = title;
+        existingBlog.content = content;
+      }
+    },
+    blogDeleted: (state, action) => {
+      const { id } = action.payload;
+      state.blogs = state.blogs.filter((blog) => blog.id !== id);
+    },
+  },
+});
+
+export const selectAllBlogs = (state) => state.blogs.blogs;
+export const selectBlogById = (state, blogId) =>
+  state.blogs.blogs.find((blog) => blog.id === blogId);
+
+export const { blogAdded, blogUpdated, blogDeleted } = blogsSlice.actions;
+export default blogsSlice.reducer;
