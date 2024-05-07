9457054e3fba196b70e6faf78aace6e96e4ad5cb
Abbas Khosravi <khosravi1977@gmail.com>
2024-05-07 08:02:01 +0430

    216

---
+++ 216/src/components/CreateBlogForm.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { nanoid } from "@reduxjs/toolkit";
+
+import { addNewBlog, blogAdded } from "../reducers/blogsSlice";
+import { selectAllUsers } from "../reducers/usersSlice";
+
+const CreateBlogForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+  const [requestStatus, setRequestStatus] = useState("idle");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onTitleChange = (e) => setTitle(e.target.value);
+  const onContentChange = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
+  const users = useSelector(selectAllUsers);
+  const canSave =
+    [title, content, userId].every(Boolean) && requestStatus === "idle";
+
+  const handleSubmitForm = async () => {
+    if (canSave) {
+      try {
+        setRequestStatus("pending");
+        await dispatch(
+          addNewBlog({
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            user: userId,
+            reactions: {
+              thumbsUp: 0,
+              hooray: 0,
+              heart: 0,
+              rocket: 0,
+              eyes: 0,
+            },
+          })
+        );
+        setTitle("");
+        setContent("");
+        setUserId("");
+        navigate("/");
+      } catch (error) {
+        console.error("Failed to save the blog", error);
+      } finally {
+        setRequestStatus("idle");
+      }
+    }
+  };
+
+  return (
+    <section>
+      <h2>ساخت پست جدید</h2>
+      <form autoComplete="off">
+        <label htmlFor="blogTitle">عنوان پست :</label>
+        <input
+          type="text"
+          id="blogTitle"
+          name="blogTitle"
+          value={title}
+          onChange={onTitleChange}
+        />
+
+        <label htmlFor="blogAuthor">نویسنده :</label>
+        <select id="blogAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value="">انتخاب نویسنده</option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.fullname}
+            </option>
+          ))}
+        </select>
+
+        <label htmlFor="blogContent">محتوای اصلی :</label>
+        <textarea
+          id="blogContent"
+          name="blogContent"
+          value={content}
+          onChange={onContentChange}
+        />
+        <button type="button" onClick={handleSubmitForm} disabled={!canSave}>
+          ذخیره پست
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default CreateBlogForm;
