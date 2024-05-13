fcebb5827a2da166f61d16632216944487595ab4
Abbas Khosravi <khosravi1977@gmail.com>
2024-05-13 08:10:05 +0430

    228

---
+++ 228/src/components/ReactionButtons.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "../reducers/blogsSlice";
+
+const reactionEmoji = {
+  thumbsUp: "👍",
+  hooray: "🎉",
+  heart: "❤️",
+  rocket: "🚀",
+  eyes: "👀",
+};
+const ReactionButtons = ({ blog }) => {
+  const dispatch = useDispatch();
+  const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+    return (
+      <button
+        key={name}
+        type="button"
+        onClick={() =>
+          dispatch(reactionAdded({ blogId: blog.id, reaction: name }))
+        }
+        className="muted-button reaction-button"
+      >
+        {emoji} {blog.reactions[name]}
+      </button>
+    );
+  });
+  return <div>{reactionButtons}</div>;
+};
+export default ReactionButtons;
