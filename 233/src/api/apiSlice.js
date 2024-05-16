224d0396cb397db071337af6093d192c8f39535b
Abbas Khosravi <khosravi1977@gmail.com>
2024-05-16 08:06:39 +0330

    233

---
+++ 233/src/api/apiSlice.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+export const apiSlice = createApi({
+  reducerPath: "api", //state.api
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
+  tagTypes: ["BLOG"],
+  endpoints: (builder) => ({
+    getBlogs: builder.query({
+      query: () => "/blogs",
+      providesTags: ["BLOG"],
+    }),
+    getBlog: builder.query({
+      query: (initialBlogId) => `/blogs/${initialBlogId}`,
+    }),
+    addNewBlog: builder.mutation({
+      query: (initialBlog) => ({
+        url: "/blogs",
+        method: "POST",
+        body: initialBlog,
+      }),
+      invalidatesTags: ["BLOG"],
+    }),
+  }),
+});
+export const { useGetBlogsQuery, useGetBlogQuery, useAddNewBlogMutation } =
+  apiSlice;
