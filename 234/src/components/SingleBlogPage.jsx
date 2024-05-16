7b827244da0d81c29dbc3004d7d8fd88c1d3a930
Abbas Khosravi <khosravi1977@gmail.com>
2024-05-16 08:08:24 +0330

    234

---
+++ 234/src/components/SingleBlogPage.jsx
@@ -0,0 +1,59 @@
+import { useParams, Link, useNavigate } from "react-router-dom";
+import ShowTime from "./ShowTime";
+import ShowAuthor from "./ShowAuthor";
+import ReactionButtons from "./ReactionButtons";
+import { useGetBlogQuery } from "../api/apiSlice";
+
+const SingleBlogPage = () => {
+  const { blogId } = useParams();
+  const { data: blog, isFetching, isSuccess } = useGetBlogQuery(blogId);
+
+  const navigate = useNavigate();
+
+  if (!blog) {
+    return (
+      <section>
+        <h2>پستی که دنبالش می گردی وجود ندارد 🤗</h2>
+      </section>
+    );
+  }
+
+  let content;
+  if (isFetching) {
+    content = <Spinner text="در حال بارگذاری ..." />;
+  } else if (isSuccess) {
+    content = (
+      <article className="blog">
+        <h2>{blog.title}</h2>
+
+        <div style={{ marginTop: "10px", marginRight: "20px" }}>
+          <ShowTime timestamp={blog.date} />
+          <ShowAuthor userId={blog.user} />
+        </div>
+
+        <p className="blog-content">{blog.content}</p>
+
+        <ReactionButtons blog={blog} />
+
+        <Link to={`/editBlog/${blog.id}`} className="button">
+          ویرایش پست
+        </Link>
+        <button className="muted-button" style={{ marginRight: "10px" }}>
+          حذف پست
+        </button>
+      </article>
+    );
+  }
+
+  const handleDelete = () => {
+    if (blog) {
+      // dispatch(deleteApiBlog(blog.id));
+      // dispatch(blogDeleted({ id: blog.id }));
+      navigate("/");
+    }
+  };
+
+  return <section> {content} </section>;
+};
+
+export default SingleBlogPage;
