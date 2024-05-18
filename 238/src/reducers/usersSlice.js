27798c192daf3495d6bba667afb24d73f0da634b
Abbas Khosravi <khosravi1977@gmail.com>
2024-05-18 08:16:22 +0430

    238

---
+++ 238/src/reducers/usersSlice.js
@@ -0,0 +1,73 @@
+import { nanoid } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+  createSelector,
+} from "@reduxjs/toolkit";
+import { createUser, deleteUser, getAllUsers } from "../services/blogsServices";
+import { apiSlice } from "../api/apiSlice";
+
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+      getUsers: builder.query({
+          query: () => "/users",
+      }),
+  }),
+});
+
+
+export const selectUsersResult = apiSlice.endpoints.getUsers.select();
+const emptyUsers = [];
+export const selectAllUsers = createSelector(
+  selectUsersResult,
+  (usersResult) => usersResult?.data ?? emptyUsers
+);
+export const selectUserById = createSelector(
+  selectAllUsers,
+  (state, userId) => userId,
+  (users, userId) => users.find((user) => user.id === userId)
+);
+
+// const userAdaptor = createEntityAdapter();
+// const initialState = userAdaptor.getInitialState();
+
+// export const fetchUsers = createAsyncThunk("/users/fetchUsers", async () => {
+//   const response = await getAllUsers();
+//   return response.data;
+// });
+
+// export const deleteApiUser = createAsyncThunk(
+//   "/users/deleteApiUser",
+//   async (initialUserId) => {
+//     await deleteUser(initialUserId);
+//     return initialUserId;
+//   }
+// );
+
+// export const addNewUser = createAsyncThunk(
+//   "/users/addNewUser",
+//   async (initialUser) => {
+//     const response = await createUser(initialUser);
+//     return response.data;
+//   }
+// );
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: emptyUsers,
+  reducers: {},
+  // extraReducers: (builder) => {
+  //     builder
+  //         .addCase(fetchUsers.fulfilled, userAdaptor.setAll)
+  //         .addCase(addNewUser.fulfilled, userAdaptor.addOne)
+  //         .addCase(deleteApiUser.fulfilled, userAdaptor.removeOne);
+  // },
+});
+
+// export const { selectAll: selectAllUsers, selectById: selectUserById } =
+//     userAdaptor.getSelectors((state) => state.users);
+
+export const { useGetUsersQuery } = extendedApiSlice;
+
+export default usersSlice.reducer;
