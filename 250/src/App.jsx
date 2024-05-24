7459a0ca8092f757fc250418ca5b62709b7fa4f2
Abbas Khosravi <khosravi1977@gmail.com>
2024-05-24 09:12:54 +0330

    250

---
+++ 250/src/App.jsx
@@ -0,0 +1,20 @@
+import { Helmet } from "react-helmet";
+import MainLayout from "./components/layouts/MainLayout";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <MainLayout>
+      <Helmet>
+        <title>فروشگاه استیکر برنامه نویسی</title>
+      </Helmet>
+
+      <div className="mx-auto max-w-6xl">
+        <Header />
+        {/* ProductList */}
+      </div>
+    </MainLayout>
+  );
+}
+
+export default App;
