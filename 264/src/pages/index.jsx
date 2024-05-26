0e77aad0cb2ec35f287e4f1976e95d95c8360984
Abbas Khosravi <khosravi1977@gmail.com>
2024-05-26 09:53:06 +0430

    264

---
+++ 264/src/pages/index.jsx
@@ -0,0 +1,47 @@
+import { useCallback, useState } from "react";
+import styles from "./index.module.css";
+import Counter from "../components/Counter";
+import throttle from "lodash.throttle";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const logCount = (count) => {
+    console.log(`INCREMENT 🐔: ${count} `);
+  };
+
+  const throttledLog = useCallback(throttle(logCount, 1000), []);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+    // logCount(count);
+    throttledLog(count);
+  };
+
+  const decreaseCount = () => {
+    if (count === 0) {
+      return alert("مقدار شمارنده نمی تواند منفی باشد ");
+    }
+
+    setCount(count - 1);
+  };
+  const resetCount = () => {
+    setCount(0);
+  };
+  const myStyle = { color: "blue", border: "1px solid green" };
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        {/* <h1 style={ {color: 'aqua', border: '1px solid red'}}>شمارنده من</h1> */}
+        <h1 style={myStyle}>شمارنده من</h1>
+      </header>
+      <Counter
+        inc={increaseCount}
+        dec={decreaseCount}
+        reset={resetCount}
+        count={count}
+      />
+    </div>
+  );
+};
+export default App;
