95b2de6c487284fa46a55195813094232554aec3
Abbas Khosravi <khosravi1977@gmail.com>
2024-05-29 11:39:43 +0430

    271

---
+++ 271/app/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+    <main>
+      <h1>سلام دوست من</h1>
+      {/* <Link href="/about" replace  prefetch={false}>درباره من</Link> */}
+      {/* <Link href={`/blog/${post.id}`}>لینک</Link> */}
+      <Link href={{ pathname: "/about", query: { name: "abbas" }, }}>
+        درباره من
+      </Link>
+    </main>
+  );
+}
