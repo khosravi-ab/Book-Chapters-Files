89bb4994e6eaed5f812aebffe2cbce051f37e6ae
Abbas Khosravi <khosravi1977@gmail.com>
2024-06-10 09:24:09 +0430

    297

---
+++ 297/middleware.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  // return NextResponse.redirect(new URL("/quiz", request.url));
+  // return NextResponse.rewrite(new URL("/under-construction", request.url));
+  if (request.nextUrl.pathname.startsWith("/about")) {
+    return NextResponse.redirect(new URL("/quiz", request.url));
+  }
+}
+
+// export const config = {
+//     matcher: "/about",
+// };
+
+// export const config = {
+//   matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+// };
