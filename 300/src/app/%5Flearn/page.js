9949a104501e8dffc3ba20090e29c533bc75740e
Abbas Khosravi <khosravi1977@gmail.com>
2024-06-13 08:33:29 +0330

    300

---
+++ 300/src/app/%5Flearn/page.js
@@ -0,0 +1,7 @@
+export default function Learn() {
+    return (
+        <div className="text-center">
+            <h3 className="text-5xl text-cyan-300 mt-10">آموزش 🔥</h3>
+        </div>
+    );
+}
